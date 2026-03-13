e3ce441ce738fd352a7b27a68603a0abe44025aa
fpols <fpolski22@gmail.com>
2026-03-13 10:41:47 +0100

    Poprawa sali 1 i 3

--- sala1.php
+++ sala1.php
@@ -1,76 +1,71 @@
 <!DOCTYPE html>
 <html lang="pl">
 <head>
-
     <meta charset="UTF-8">
     <title>Układ Sali Kinowej</title>
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+
 <div id="menu">
-
-    <nav>
-        <ol><a href="sala2.php">Sala2</a></ol><ol><a href="sala3.php">Sala3</a></ol><ol><a href="sala4.php">Sala4</a></ol>
-
-    </nav>
-
+    <div class="navigation">
+        <ol><a href="sala2.php">Sala2</a></ol>
+        <ol><a href="sala3.php">Sala3</a></ol>
+        <ol><a href="sala4.php">Sala4</a></ol>
+    </div>
 </div>
-
-
-
 
 <div id="print-area">
-       <div class="screen">Ekran</div>
-    <div class="cinema-hall">
-
-
-
+    <div class="screen">Ekran</div>
+    <div class="cinema-hall" id="c">
         <?php
-        $rows= [
-                1 => range(11,1),
-                2=> range(15,1),
-                3=>range(15,1),
-                4=>range(15,1),
-                5=>range(15,1),
-                6=>range(15,1),
-                7=>range(15,1),
-                8=>range(19,1),
+            $rows= [
+                    1 => range(15,1),
+                    2 => range(15,1),
+                    3 => range(15,1),
+                    4 => range(15,1),
+                    5 => range(15,1),
+                    6 => range(15,1),
+                    7 => range(15,1),
+                    8 => range(19,1),
+            ];
 
+            $counter = 0;
+            foreach($rows as $rowNum => $seats) {
+                echo "<div class='row row-$rowNum'>";
+                echo "<div class='row-number'>$rowNum</div>";
 
+                foreach ($seats as $seatNum) {
+                    $uniqueId = "s_" . $counter++;
 
-
-
-
-        ];
-
-        foreach($rows as $rowNum => $seats) {
-            echo "<div class='row row-$rowNum'>";
-
-            echo "<div class='row-number'>$rowNum</div>";
-
-            foreach ($seats as $seatNum) {
-                echo "<div class='seat' title='Rząd $rowNum ,Miejsce $seatNum'>$seatNum</div>";
-            }
-
-            echo "<div class='row-number'>$rowNum</div>";
-
-            echo "</div>";
-        }
+                    if ($rowNum == 1 && ($seatNum >= 7 && $seatNum <= 10)) {
+                        echo "<div class='unavailableSeat' title='Rząd $rowNum, Miejsce $seatNum'></div>";
+                    } else if ($rowNum == 1 && ($seatNum > 10)) {
+                        echo "<div class='seat' title='Rząd $rowNum, Miejsce $seatNum'>";
+                        echo "  <input type='checkbox' class='inputClass' id='$uniqueId' name='Miejsca'>";
+                        $result = $seatNum - 4;
+                        echo "  <label for='$uniqueId' class='seat-label'>$result</label>";
+                        echo "</div>";
+                    } else {
+                        echo "<div class='seat' title='Rząd $rowNum, Miejsce $seatNum'>";
+                        echo "  <input type='checkbox' class='inputClass' id='$uniqueId' name='Miejsca'>";
+                        echo "  <label for='$uniqueId' class='seat-label'>$seatNum</label>";
+                        echo "</div>";
+                    }
+                }
+                    echo "<div class='row-number'>$rowNum</div>";
+                    echo "</div>";
+                }
         ?>
-
-
     </div>
-
-
+</div>
+<hr>
+<div id="actions">
+    <button onclick="printDiv()">Drukuj</button>
 </div>
 
-<button onclick="window.print()">Drukuj</button>
-
-<a style="background-color: #29abe2; text-decoration: none; border: solid black 2px"  href="stronaglowna.php">Strona główna</a>
-
-
+    <script src="druk.js"></script>
+    <script src="https://unpkg.com/selecto/dist/selecto.min.js"></script>
+    <script type="module" src="Operations.js"></script>
 </body>
-
-
-
 </html>
--- sala3.php
+++ sala3.php
@@ -8,59 +8,65 @@
 <body>
 
 <div id="menu">
-
-    <nav>
-        <ol><a href="sala1.php">Sala1</a></ol><ol><a href="sala2.php">Sala2</a></ol><ol><a href="sala4.php">Sala4</a></ol>
-
-    </nav>
-
+    <div class="navigation">
+        <ol><a href="sala1.php">Sala1</a></ol>
+        <ol><a href="sala2.php">Sala2</a></ol>
+        <ol><a href="sala4.php">Sala4</a></ol>
+    </div>
 </div>
 
+<div id="print-area">
+    <div class="screen">Ekran</div>
+    <div class="cinema-hall" id="c">
+        <?php
+        $rows = [
+                1  => range(1, 23),
+                2  => range(1, 26),
+                3  => range(1, 27),
+                4  => range(1, 26),
+                5  => range(1, 27),
+                6  => range(1, 26),
+                7  => range(1, 27),
+                8  => range(1, 26),
+                9  => range(1, 27),
+                10 => range(1, 26),
+                11 => range(1, 27),
+                12 => range(1, 26),
+                13 => range(1, 27),
+                14 => range(1, 33),
+        ];
 
+        $counter = 0;
+        foreach ($rows as $rowNum => $seats) {
+            // Start bloku z miejscami
+            echo "<div class='row row-$rowNum'>";
+            // Numery rzędu z lewej
+            echo "<div class='row-number'>$rowNum</div>";
 
+            foreach ($seats as $seatNum) {
+                $uniqueId = "s_" . $counter++;
 
+                echo "<div class='seat' title='Rząd $rowNum, Miejsce $seatNum'>";
+                echo "  <input type='checkbox' class='inputClass' id='$uniqueId' name='Miejsca'>";
+                echo "  <label for='$uniqueId' class='seat-label'>$seatNum</label>";
+                echo "</div>";
+            }
 
-<div class="screen">Ekran</div>
-
-<div class="cinema-hall">
-    <?php
-    // Definicja liczby miejsc w każdym rzędzie na podstawie obrazka
-    $rows = [
-        1  => range(1, 23),
-        2  => range(1, 26),
-        3  => range(1, 27),
-        4  => range(1, 26),
-        5  => range(1, 27),
-        6  => range(1, 26),
-        7  => range(1, 27),
-        8  => range(1, 26),
-        9  => range(1, 27),
-        10 => range(1, 26),
-        11 => range(1, 27),
-        12 => range(1, 26),
-        13 => range(1, 27),
-        14 => range(1, 33),
-    ];
-
-    foreach ($rows as $rowNum => $seats) {
-        echo "<div class='row row-$rowNum'>";
-
-        // Numer rzędu z lewej
-        echo "<div class='row-number'>$rowNum</div>";
-
-        // Generowanie miejsc
-        foreach ($seats as $seatNum) {
-            echo "<div class='seat' id='' title='Rząd $rowNum, Miejsce $seatNum'>$seatNum</div>";
+            // Numer rzędu z prawej
+            echo "<div class='row-number'>$rowNum</div>";
+            // Zakończenie bloku z miejscami
+            echo "</div>";
         }
-
-        // Numer rzędu z prawej
-        echo "<div class='row-number'>$rowNum</div>";
-
-        echo "</div>";
-    }
-    ?>
+        ?>
+    </div>
+</div>
+<hr>
+<div id="actions">
+    <button onclick="printDiv()">Drukuj</button>
 </div>
 
-<a style="border: solid black 2px;border-radius:2px;background-color: #29abe2;text-decoration: none; " href="stronaglowna.php">Strona główna</a>
+<script src="druk.js"></script>
+<script src="https://unpkg.com/selecto/dist/selecto.min.js"></script>
+<script type="module" src="Operations.js"></script>
 </body>
 </html>
